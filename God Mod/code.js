b172e8ae767f6406f438b1e410254e70f88dcdf4
NoComp <103075692+thetruerailen@users.noreply.github.com>
2023-06-20 12:30:14 -0500

    Create code.js

---
+++ God Mod/code.js
@@ -0,0 +1,2 @@
+permStore = webpackChunkdiscord_app.push([[Symbol()], {}, e => Object.values(e.c)]).find(m => m?.exports?.Z?.canBasicChannel).exports.Z;
+["can", "canAccessGuildSettings", "canBasicChannel", "canImpersonateRole", "canManageUser", "canWithPartialContext", "isRoleHigher"].forEach(a => permStore.__proto__[a] = () => true);
